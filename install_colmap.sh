619ad389d7cadd39b2188a781a0ab5ad60f8277f
Alexander Veicht <veichta@ethz.ch>
2023-03-21 13:11:00 +0100

    Fix FLANN hints.

--- install_colmap.sh
+++ install_colmap.sh
@@ -47,8 +47,8 @@ ctest
 make install
 
 # add path hints for colmap
-sed -i 's/^set(FLANN_INCLUDE_DIR_HINTS.*/set(FLANN_INCLUDE_DIR_HINTS ${INSTALL_DIR}include)/' ${INSTALL_DIR}share/colmap/COLMAPConfig.cmake
-sed -i 's/^set(FLANN_LIBRARY_DIR_HINTS.*/set(FLANN_LIBRARY_DIR_HINTS ${INSTALL_DIR}lib)/' ${INSTALL_DIR}share/colmap/COLMAPConfig.cmake
+sed -i 's/^set(FLANN_INCLUDE_DIR_HINTS.*/set(FLANN_INCLUDE_DIR_HINTS "${INSTALL_DIR}include")/' ${INSTALL_DIR}share/colmap/COLMAPConfig.cmake
+sed -i 's/^set(FLANN_LIBRARY_DIR_HINTS.*/set(FLANN_LIBRARY_DIR_HINTS "${INSTALL_DIR}lib")/' ${INSTALL_DIR}share/colmap/COLMAPConfig.cmake
 
 cd $CURRDIR
 
